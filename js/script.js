f0280cd76d6e30b4ea3ccca1baa5ff0ea8365602
jtsapos <jtsapos@gmx.com>
2023-04-01 16:15:11 -0400

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,57 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// For assistance: 
+  // Check the "Project Resources" section of the project instructions
+  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/*** 
+ * `quotes` array 
+***/
+const quotes = [
+  {
+    quote: 'The journey of a thousand miles begins with one step',
+    source: "Lao Tzu"
+  },
+  {
+    quote: 'Those who dare to fail miserably can achieve greatly',
+    source: "John F. Kennedy"
+  },
+  {
+    quote: 'Wisely, and slow. They stumble that run fast',
+    source: "William Shakespeare"
+  },
+  {
+    quote: 'Suffer the little children to come unto me',
+    source: "Jesus Christ"
+  },
+  {
+    quote: 'Family is the most important thing in the world',
+    source: "Princess Diana"
+  }
+]
+
+
+/***
+ * `getRandomQuote` function
+***/
+// Function that generates a random quote
+function randomQuote() {
+  return Math.floor(Math.random() * 100) + 1;
+}
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
